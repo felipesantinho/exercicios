637cb2ca9b12f59f98299d5b4d762242f32e15c4
felipesantinho <felipeaugustosantinho@gmail.com>
2022-09-28 13:57:07 -0300

    getelementbyid

---
+++ ex1/script.js
@@ -0,0 +1,13 @@
+//getElementById()
+
+let main_title = document.getElementById('main_title')
+console.log(main_title)
+
+let subtitle = document.getElementById('subtitle')
+console.log(subtitle)
+
+let paragraph_one = document.getElementById('paragraph_one')
+console.log(paragraph_one)
+
+let paragraph_two = document.getElementById('paragraph_two')
+console.log(paragraph_two)
