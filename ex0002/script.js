0a5c41aa0702d68b366ddbf629c557dc1bcf3ff8
felipesantinho <felipeaugustosantinho@gmail.com>
2022-09-28 14:15:22 -0300

    getElementsByClassName

---
+++ ex0002/script.js
@@ -0,0 +1,17 @@
+//getElementsByClassName()
+
+let main_title = document.getElementsByClassName('main_title')
+console.log(main_title)
+
+let subtitle = document.getElementsByClassName('subtitle')
+console.log(subtitle)
+
+/*
+
+let paragraph = document.getElementsByClassName('paragraph')
+console.log(paragraph)
+
+*/
+
+let paragraph = document.getElementsByClassName('paragraph')[0]
+console.log(paragraph)
